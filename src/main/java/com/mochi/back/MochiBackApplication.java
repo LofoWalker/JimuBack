96532530935da2f65b7124a43734799215ad4660
Tom Walker <twalker.ext@galerieslafayette.com>
2021-04-08 22:36:43 +0200

    Création Mochi Back

---
+++ src/main/java/com/mochi/back/MochiBackApplication.java
@@ -0,0 +1,13 @@
+package com.mochi.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MochiBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MochiBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mochi/back/MochiBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mochi.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MochiBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
